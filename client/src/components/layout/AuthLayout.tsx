2764010c263843459a4ac6dce132aa28c9f81d17
Hyerim Yang <hlhlstar@naver.com>
2024-11-07 20:13:07 +0900

    Chore: Vite+React+TS 프로젝트 전체를 `client/` 디렉토리로 이동
    - 프로젝트 구조를 `client/`와 `server/`로 분리하기 위해 기존 모든 내용을 `client/`로 이동

--- client/src/components/layout/AuthLayout.tsx
+++ client/src/components/layout/AuthLayout.tsx
@@ -7,10 +7,12 @@ const AuthLayout = () => {
   return (
     <main className="w-screen h-screen flex justify-center items-center bg-gray200">
       <div className="w-[30rem] p-6 rounded-lg bg-white shadow-md">
-        <p className="font-bold text-[2rem] text-center">AUTH TODO</p>
-        <p className="font-semibold text-center">
-          {page} 후 나만의 할일을 관리해보세요
-        </p>
+        <div className="text-center space-y-1">
+          <p className="font-bold text-[2rem]">AUTH TODO</p>
+          <p className="font-semibold">
+            {page} 후 나만의 TODO를 관리해 보세요! ✅
+          </p>
+        </div>
         <Outlet />
       </div>
     </main>
