9d6a0bc207cf4255b4d0586c3c92851f53027f7f
Hyerim Yang <hlhlstar@naver.com>
2024-12-13 22:45:07 +0900

    feat: React Query 설정 추가 및 Devtools 개발 환경에서만 렌더링되도록 적용
    - QueryClient 생성 및 Provider 구성
    - React Query Devtools 추가 및 조건부 렌더링 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRouter from "./router/AppRouter";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <AppRouter />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRouter />
+      {import.meta.env.MODE === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
+  );
 };
 
 export default App;
