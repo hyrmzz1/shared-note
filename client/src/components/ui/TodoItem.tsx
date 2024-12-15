48c51039ace91e664bf91ee7a85e836a16268b17
Hyerim Yang <hlhlstar@naver.com>
2024-12-15 18:37:10 +0900

    refactor: 불필요한 주석 제거

--- client/src/components/ui/TodoItem.tsx
+++ client/src/components/ui/TodoItem.tsx
@@ -7,7 +7,6 @@ interface TodoItemProps {
 
 const TodoItem = ({ item, isSelected }: TodoItemProps) => {
   return (
-    // outline outline-offset-2 outline-blue
     <div
       className={`p-4 rounded-xl shadow-md bg-[#ebf1ff] hover:bg-[#d7e3ff] ${
         isSelected ? "outline outline-offset-2 outline-blue" : ""
