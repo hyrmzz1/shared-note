4ed1344d8c07085383a0da08e4847d3956eda795
Hyerim Yang <hlhlstar@naver.com>
2024-12-18 17:50:28 +0900

    feat: `refetchOnWindowFocus` 옵션 비활성화
    - 기본 쿼리 옵션에서 `refetchOnWindowFocus`를 `false`로 설정
    - 윈도우가 다시 포커스될 때 불필요한 데이터 호출 방지
    - 불필요한 네트워크 요청을 줄임

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRouter from "./router/AppRouter";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을 때 데이터를 호출할 것인지
+    },
+  },
+});
 
 const App = () => {
   return (
