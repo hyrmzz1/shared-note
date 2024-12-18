dbe483a99f328d19cfd42cd9362aefa02abc7dad
Hyerim Yang <hlhlstar@naver.com>
2024-12-18 22:00:14 +0900

    feat: react-toastify 설치 및 전역 에러 알림 처리 구현
    - react-toastify 설치 및 ToastContainer 추가
    - QueryClient 설정에 글로벌 에러 핸들링 로직 추가
      - QueryCache와 MutationCache의 onError를 활용해 발생한 에러를 토스트 메시지로 표시
        - 초기 데이터 로드 실패와 배경 업데이트 실패를 구분해 알림 표시
        - Mutation 에러 발생 시 토스트 알림 처리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import {
+  MutationCache,
+  QueryCache,
   QueryClient,
   QueryClientProvider,
   useQueryErrorResetBoundary,
@@ -6,9 +8,24 @@ import {
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorInfo } from "react";
+import { Bounce, toast, ToastContainer } from "react-toastify";
 import AppRouter from "./router/AppRouter";
 
 const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (query.state.data !== undefined) {
+        // background error 발생 (캐시에 데이터가 있는 경우)
+        toast.error(`Background update failed: ${error.message}`);
+      } else {
+        // 초기 데이터 로드 실패 (캐시에 데이터가 없는 경우)
+        toast.error(`Failed to load data: ${error.message}`);
+      }
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error) => toast.error(`Mutation Error: ${error.message}`),
+  }),
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을 때 데이터를 호출할 것인지
@@ -33,6 +50,19 @@ const App = () => {
       >
         <AppRouter />
       </ErrorBoundary>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        transition={Bounce}
+      />
       {import.meta.env.MODE === "development" && (
         <ReactQueryDevtools initialIsOpen={false} />
       )}
